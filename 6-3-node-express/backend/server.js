1a0a3bda5bbe3d91afcb14126d1dcddd32e4252f
dana <157533000+Dana11a@users.noreply.github.com>
2026-04-06 12:54:34 +0300

    TODO2

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,10 +1,9 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 
+// TODO 1: Define server port
 const app = express();
 const PORT = 3000;
-// TODO 1: Define server port
-
 
 // TODO 4: Import and Apply CORS middleware
 
