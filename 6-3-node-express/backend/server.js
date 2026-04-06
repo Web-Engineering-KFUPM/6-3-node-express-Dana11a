f841a68e0d07aa7dae1921658b8f6733e2eff2ee
dana <157533000+Dana11a@users.noreply.github.com>
2026-04-06 13:11:40 +0300

    TODO6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,8 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 // TODO 1: Define server port
 const app = express();
@@ -11,11 +13,17 @@ app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 
-
+app.use(cors());
+app.use(morgan("dev"));
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+    res.send("Random Quote API is running ");
+  });
 
 // TODO 6.2: Create "/api/quote" route
-
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+  });
 
 // TODO 7: Start server using app.listen
